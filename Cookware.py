c1cee584cc365bf782f20c339955185e585d7730
BAHER4 <151413521+BAHER4@users.noreply.github.com>
2024-04-23 11:57:01 -0400

    Add files via upload

--- Cookware.py
+++ Cookware.py
@@ -3,7 +3,17 @@ from Container import Container
 class Cookware(Container):
     def __init__(self, pos, shape):
         super().__init__(pos, shape)
+        self.state = "no-heat"
 
+    def cooking(self):
+        if self.ingredients != []:  
+            self.state = "heat"
+            print("Le pot a cuit miam :)")
+        else:
+            print("Le pot n'a pas cuit triste :(")
+
+    def draw(self, screen):
+            super().draw(screen) 
 
     def update_position(self, player_x, player_y):
         return super().update_position(player_x, player_y)
